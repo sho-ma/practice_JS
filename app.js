f8f22e46d62aaee37984eecee975fa448186a67b
sho-ma <shomaegawa510@gmail.com>
2021-08-07 08:14:08 +0000

    this

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.bg1').on('click', function(){
+    $('.bg1').slideUp();
+  });
+
+  $('.bg2').on('click', function(){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function(){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function(){
+    $('.bg4').slideUp();
+  });
 });
